9fca44c90aed51cfceb5a37a211ac6e069a6652d
AngelFox24 <curilaurente@gmail.com>
2020-06-03 16:54:32 -0500

    Updates

---
+++ precache-manifest.4db166cadefc5531233bee116dc6e069.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d0cc6556cd09f51b4629dd5e7f19205",
+    "url": "/teprestope/index.html"
+  },
+  {
+    "revision": "7d8eef246a714d3794e2",
+    "url": "/teprestope/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "763671ee151d4f976a86",
+    "url": "/teprestope/static/js/2.25f9a3b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/teprestope/static/js/2.25f9a3b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d8eef246a714d3794e2",
+    "url": "/teprestope/static/js/main.c0e5f81d.chunk.js"
+  },
+  {
+    "revision": "5db822bf8cb0aa929d6f",
+    "url": "/teprestope/static/js/runtime-main.c9181c79.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/teprestope/static/media/logo.5d5d9eef.svg"
+  }
+]);
